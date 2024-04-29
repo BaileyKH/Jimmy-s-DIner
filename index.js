67506566a4488a61f31f2db231006dbea04f516a
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-29 07:37:01 -0700

    add menu items and calculate total price

--- index.js
+++ index.js
@@ -26,14 +26,55 @@ const menuArray = [
 document.addEventListener("click", (e) => {
     if (e.target.dataset.add){
         handleAddItem(e.target.dataset.add)
+    } else if (e.target.dataset.remove){
+        handleRemoveItem(e.target.dataset.remove)
     }
 })
 
+let orderArray = []
+
 function handleAddItem(menuID){
+
     const targetMenuItem = menuArray.filter((items) => {
-        return items.id == menuID
+        return items.id === Number(menuID)
+    })[0]
+    orderArray.push(targetMenuItem)
+
+    document.getElementById("buy-item").innerHTML += `
+        <div class="order-container">
+            <div class="order-content>
+                <div class="order-name">
+                    <p>${targetMenuItem.name}</p>
+                    <p class="remove" data-remove="${menu.id}">Remove</p>
+                </div>
+                <div class="order-price">
+                    <p>$${targetMenuItem.price}</p>
+                </div>
+            </div>
+        </div>`
+
+
+    const orderTotal = orderArray.reduce((total, current) => total + current.price, 0)
+    document.getElementById("total").innerHTML = `
+        <div>
+            <p>Total: $${orderTotal.toFixed(2)}</p>
+        </div>`
+    
+    
+    render()
+}
+
+function handleRemoveItem(menuID) {
+    const targetMenuItem = menuArray.filter((items) => {
+        return items.id === Number(menuID)
     })[0]
 
+    let orderIndex = orderArray.findIndex(targetMenuItem)
+    if(orderIndex !== -1){
+        orderArray.splice(orderIndex, 1)
+    }
+    console.log(orderArray)
+    render()
 }
 
 function getMenuHtml() {
@@ -48,7 +89,7 @@ function getMenuHtml() {
                         <img src="${menu.image}"/>
                         <div>
                             <p class="menu-name">${menu.name}</p>
-                            <p class="menu-ingredients">${menu.ingredients}</p>
+                            <p class="menu-ingredients">${menu.ingredients.join(", ")}</p>
                             <p class="menu-price">$${menu.price}</p>
                         </div>
                     </div>
